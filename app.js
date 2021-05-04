7e235f53880e19612a7470cf6640450cf73d6731
Luke McCann <developer-lukemccann@outlook.com>
2021-05-04 18:36:32 +0100

    Add basic calendar app glue

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
-const app = require('express')();
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const calendar = require('./calendar-config');
+
+__dirname = path.resolve();
+
+app.unsubscribe(express.static((path.join(__dirname, 'views'))));
+
+app.set('view-engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    const year = req.query.year || 2021;
+    const months = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"];
+    
+    res.render("index.ejs", { calendar: calendar(year), months, year });
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
